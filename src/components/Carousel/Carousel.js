e5db885ceb8c11efdd288a087eedf7a3d0530bcb
Rekha <baburekha002691@gmail.com>
2021-04-18 23:29:23 +0530

    Carousel of Products

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Carousel.css";
+import useCarouselHook from "./CarouselHook";
+
+const Carousel = () => {
+  const {
+    products,
+    nextSlide,
+    prevSlide
+  } = useCarouselHook();
+  return (
+    <React.Fragment>
+      <div className="carousel">
+        <i id="chevron-arrow-left" onClick={prevSlide} />
+        <ol className="carousel-container">
+          {products.map((product) => {
+            return (
+              <li key={product._id} className="carousel-item">
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
+                <div className="card-body">
+                  <h2>{product.name}</h2>
+                  <p>{product.category}</p>
+                  <div className="price">${product.price}</div>
+                </div>
+              </li>
+            );
+          })}
+        </ol>
+        <i id="chevron-arrow-right" onClick={nextSlide} />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Carousel;
---
+++ src/data.js
@@ -0,0 +1,131 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nile Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120
+        },
+        {
+            _id: '2',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 78
+        },
+        {
+            _id: '3',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100
+        },
+        {
+            _id: '4',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 65
+        },
+        {
+            _id: '5',
+            name: 'Adidas Sli Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 139
+        },
+        {
+            _id: '6',
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 220
+        },
+        {
+            _id: '7',
+            name: 'Tombik Sandals',
+            category: 'Sandals',
+            image: '/images/Sandals4.jpg',
+            price: 23
+        },
+        {
+            _id: '8',
+            name: 'Troadlop Kids Sneaker',
+            category: 'Shoes',
+            image: '/images/Shoes2.jpg',
+            price: 65
+        },
+        {
+            _id: '9',
+            name: 'COODO Sports Sneakers',
+            category: 'Shoes',
+            image: '/images/Shoes3.jpg',
+            price: 28
+        },
+        {
+            _id: '10',
+            name: 'Nautica Womens Wallet',
+            category: 'Wallets',
+            image: '/images/Wallet1.jpg',
+            price: 19
+        },
+        {
+            _id: '11',
+            name: 'Dickies Men\'s chain Wallet',
+            category: 'Wallets',
+            image: '/images/Wallet2.jpg',
+            price: 18
+        },
+        {
+            _id: '12',
+            name: 'Travando Men\'s Slim Wallet',
+            category: 'Wallets',
+            image: '/images/Wallet3.jpg',
+            price: 29
+        },
+        {
+            _id: '13',
+            name: 'Travelambo Women\'s  Wallet',
+            category: 'Wallets',
+            image: '/images/Wallet4.jpg',
+            price: 30
+        },
+        {
+            _id: '14',
+            name: 'RUNBOX Slim Wallet',
+            category: 'Wallets',
+            image: '/images/Wallet5.jpg',
+            price: 27
+        },
+        {
+            _id: '15',
+            name: 'Men Black Open Toe Sandals',
+            category: 'Sandals',
+            image: '/images/Sandals1.jpg',
+            price: 38
+        },
+        {
+            _id: '16',
+            name: 'Men Brown Toe Leather Sandals',
+            category: 'Sandals',
+            image: '/images/Sandals2.jpg',
+            price: 20
+        },
+        {
+            _id: '17',
+            name: 'Men Grey Toe Velcro Sandals',
+            category: 'Sandals',
+            image: '/images/Sandals3.jpg',
+            price: 20
+        },
+        {
+            _id: '18',
+            name: 'Merrell Men\'s Hiking Shoe',
+            category: 'Shoes',
+            image: '/images/Shoes1.jpg',
+            price: 83
+        }
+    ]
+}
+export default data;
